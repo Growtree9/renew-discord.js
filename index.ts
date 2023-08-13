cb8b952aad27b6280c7ab8b3683c54a750c67c02
zqo <123120185+zqodev@users.noreply.github.com>
2023-08-13 23:24:15 +0200

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import {Bot} from './structures/Bot';
 import {Intent, IntentsManager} from "./managers/IntentsManager";
 
-const token: string = 'MTE0MDMzNjAwNDcyNTc0MzYxNg.G-lEKw.8-hQqt7GPfrhXJQkor-0SJExYMiQB6MhJBNonI';
+const token: string = '';
 const botIntents: Intent[] = [Intent.GUILDS, Intent.GUILD_MESSAGES, Intent.GUILD_MEMBERS];
 const intentsManager: IntentsManager = new IntentsManager(botIntents);
 const bot: Bot = new Bot(intentsManager);
